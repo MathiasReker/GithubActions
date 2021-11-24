79dc3abaefecf56b35f47bcdbdfe0a6182a7ffca
MathiasReker <mathias@reker.dk>
2021-11-24 23:43:58 +0100

    test

--- src/main/java/com/example/githubactions/domain/service/Testing.java
+++ src/main/java/com/example/githubactions/domain/service/Testing.java
@@ -2,6 +2,11 @@ package com.example.githubactions.domain.service;
 
 public class Testing {
   public boolean isAdult(int age) {
-    return age >= 18;
+               return
+
+
+        age        >=    18;
   }
+
+
 }
