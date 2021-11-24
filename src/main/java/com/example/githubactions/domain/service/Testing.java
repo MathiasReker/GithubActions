69107be999207280d5e7d97ca7866a4c62725ce4
MathiasReker <MathiasReker@users.noreply.github.com>
2021-11-24 22:44:21 +0000

    Google Java Format

--- src/main/java/com/example/githubactions/domain/service/Testing.java
+++ src/main/java/com/example/githubactions/domain/service/Testing.java
@@ -2,11 +2,6 @@ package com.example.githubactions.domain.service;
 
 public class Testing {
   public boolean isAdult(int age) {
-               return
-
-
-        age        >=    18;
+    return age >= 18;
   }
-
-
 }
