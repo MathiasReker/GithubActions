ab07059a717e7cdc4095617c150c6d890e7cb5b7
Mathias Reker <mathias@reker.dk>
2022-04-11 15:19:32 +0200

    Update FrontController.java

--- src/main/java/com/example/githubactions/FrontController.java
+++ src/main/java/com/example/githubactions/FrontController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class FrontController {
   @GetMapping("/")
   public String hello() {
-    return "Hello World8";
+    return "Hello World9";
   }
 }
